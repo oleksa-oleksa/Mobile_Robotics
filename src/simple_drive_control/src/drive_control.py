bb19b18cd037ee0f47403139d063770014919a89
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-11-15 18:23:37 +0100

    drive a circle task done

--- src/simple_drive_control/src/drive_control.py
+++ src/simple_drive_control/src/drive_control.py
@@ -12,7 +12,8 @@ from std_msgs.msg import String
 # --- definitions ---
 epsilon = 0.05   # allowed inaccuracy for distance calculation
 speed_rpm = 200
-angle_left = 30
+#angle_left = 30
+angle_left = 65
 angle_straight = 90
 angle_right = 150
 last_odom = None
