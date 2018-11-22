895fd86c14147ae3c1632af4b604865a55734b0d
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-11-22 22:55:25 +0100

    Rotation Matrix To Euler Angles

--- src/assignment4_image_processing/src/image_processing.py
+++ src/assignment4_image_processing/src/image_processing.py
@@ -1,9 +1,48 @@
 #!/usr/bin/env python
 import sys
 import cv2
+import math
 from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib.mlab import center_matrix
       
+
+def sort_pts(pts):
+    sorted_by_y = sorted(pts, key=lambda r: r[1])
+    twos = zip(*(iter(sorted_by_y),) * 2)
+    sorted_by_x = map(lambda t: sorted(t, key=lambda p: p[0]), twos)
+    return np.array([z for l in sorted_by_x for z in l])  
+
+# Checks if a matrix is a valid rotation matrix.
+def isRotationMatrix(R) :
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype = R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+ 
+# Calculates rotation matrix to euler angles
+# The result is the same as MATLAB except the order
+# of the euler angles ( x and z are swapped ).
+def rotationMatrixToEulerAngles(R) :
+ 
+    assert(isRotationMatrix(R))
+     
+    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+     
+    singular = sy < 1e-6
+ 
+    if  not singular :
+        x = math.atan2(R[2,1] , R[2,2])
+        y = math.atan2(-R[2,0], sy)
+        z = math.atan2(R[1,0], R[0,0])
+    else :
+        x = math.atan2(-R[1,2], R[1,1])
+        y = math.atan2(-R[2,0], sy)
+        z = 0
+ 
+    return np.array([x, y, z])
+
 def main(args):
     cv_image = cv2.imread("points_lab_measured.jpeg")
     #=====================================
@@ -80,21 +119,37 @@ def main(args):
     K = 6
     ret,label,center = cv2.kmeans(white_points_float, K, None, criteria,10,
                                   cv2.KMEANS_PP_CENTERS)
+   
+    center = sort_pts(center.tolist())
+    obj_points = sort_pts(obj_points.tolist())
     
     for c in center:
-        cv2.circle(cv_image, tuple(c), 4, (255, 0, 0), 2)
+        cv2.circle(cv_image, tuple(c.astype(np.int)), 
+                   4, (255, 0, 0), 2)
         
     cv2.imshow('threshold',cv_image)
-    
-    print("Clusters centers: ", center)
-    print("objects centers: ", obj_points)
-    
+        
     _, rvec, tvec = cv2.solvePnP(obj_points, center, camera_mat, dist_coeffs)
     
     #=====================================
     #Task 5: Finding the camera location and orientation
     rmat = np.zeros((3,3))
     cv2.Rodrigues(rvec, rmat, jacobian=0)
+    
+    hmat = np.identity(4, np.float32)
+    print(hmat)
+    hmat[0:3, 0:3] = rmat
+    hmat[0:3, 3] = tvec.T
+    print("Homogeneous transform:")
+    print(hmat)
+    
+    hmat_inv = np.linalg.inv(hmat)
+    print("Inverse Homogeneous transform:")
+    print(hmat_inv)
+    
+    print("Rotation Matrix To Euler Angles:")
+    angles = rotationMatrixToEulerAngles(rmat)
+    print("x: ", angles[0], "y: ", angles[1], "z: ", angles[2])
     
     print("The world coordinate system's origin in camera's coordinate system:")
     print("=== camera rvec:")
