49a556e03d7bbc082da418b35a0db523beedcc75
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-11-22 16:15:23 +0100

    rewriting code

--- src/assignment4_image_processing/src/image_processing.py
+++ src/assignment4_image_processing/src/image_processing.py
@@ -10,27 +10,12 @@ from geometry_msgs.msg import Point
 from geometry_msgs.msg import Quaternion
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-#import matplotlib
-#matplotlib.use('Agg')
 from matplotlib import pyplot as plt
-
-# from __future__ import print_function
-
-class image_converter:
-
-  def __init__(self):
-    self.image_pub = rospy.Publisher("/image_processing/bin_img",Image, queue_size=1)
-
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/app/camera/rgb/image_raw",Image,self.callback, queue_size=1)
-
-# converts ROS Image into an OpenCV compatible image
-  def callback(self,data):
-    try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-      print(e)
-
+      
+def main(args):
+    rospy.init_node('assignment4_image_proccesing', anonymous=True)
+    
+    cv_image = cv2.imread("points_lab_measured.png")
     #=====================================
     #make it gray
     #Task 2: obtain the RGB image from the camera and transform it to gray
@@ -118,22 +103,11 @@ class image_converter:
     #This is done using solvePnP().
     
     '''
-
     try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(thresh1, "mono8"))
-    except CvBridgeError as e:
-      print(e)
-      
-
-def main(args):
-  rospy.init_node('image_converter', anonymous=True)
-  ic = image_converter()
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
-
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
